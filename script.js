0e1444e4b9b8f3d768a4102aa5d003bca72437d1
garrisonma <133896022+garrisonma@users.noreply.github.com>
2024-02-23 19:13:32 +0000

    udpate

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let score = 0;
 let timerValue = 60;
 let timerInterval;
 const keywordsArray = ["dog", "cat", "bird", "fish", "elephant", "lion", "tiger", "zebra", "house", "book", "computer", "window", "man", "sock", "shoe", "taco", "hamburger", "pizza", "sushi", "chicken", "pig", "poo", "cry", "brain", "rainbow", "skull", "blood", "zombie", "clown", "alien", "swim", "angel", "angry", "mushroom", "pancake", "chocolate", "ice cream", "camera", "television", "laugh"];
-//const keywordsArray = ["book"];
+//const keywordsArray = ["peekaboo"];
 let currentName;
 
 function startGame() {
@@ -18,13 +18,13 @@ async function fetchRandomEmoji() {
   const response = await fetch(`https://emoji-api.com/emojis?search=${word}&access_key=a91c5f2c2f41ba42d6617f6fb7128fb2383c0c26`);
   const data = await response.json();
 
-  console.log('Emoji API Response:', data); 
+  console.log('Emoji API Response:', data);
 
   if (Array.isArray(data) && data.length > 0 && typeof data[0].character === 'string') {
     return data[0].character;
   } else {
     console.error('Error fetching emoji. API response:', data);
-    return ''; 
+    return '';
   }
 }
 
@@ -35,7 +35,7 @@ async function displayRandomEmoji() {
 
 function updateTimer() {
   timerValue--;
-  document.getElementById('timerDisplay').innerText = timerValue + 's';
+  document.getElementById('timerDisplay').innerText = timerValue + ' s';
 
 
   if (timerValue === 0) {
@@ -48,11 +48,11 @@ document.getElementById('inputField').addEventListener('input', function(event) 
   const userInput = event.target.value.trim();
 
 
-  if (userInput !== '' && userInput.toLowerCase() === currentName) { 
+  if (userInput !== '' && userInput.toLowerCase() === currentName) {
     score += 10;
-    document.getElementById('score').innerText = score + " points";
-    displayRandomEmoji(); 
-    document.getElementById('inputField').value = ''; 
+    document.getElementById('score').innerText = score;
+    displayRandomEmoji();
+    document.getElementById('inputField').value = '';
   }
 });
 
@@ -63,7 +63,9 @@ function endGame() {
   document.getElementById('inputField').disabled = true;
 }
 document.getElementById('restartButton').addEventListener('click', function(){
-    startGame();
+    startGame()
 })
 // Start the game when the page loads
-startGame();
+setTimeout(() => {
+    startGame();
+}, 5000);
