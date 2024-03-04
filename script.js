19ee7a61587e05d60980fe1851c2429f71d01f41
garrisonma <133896022+garrisonma@users.noreply.github.com>
2024-03-04 20:17:15 +0000

    timer

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function startGame() {
   //document.getElementById('emojiDisplay').style.display = 'block';
  // document.getElementById('scoreDisplay').style.display = 'block';
   score = 0;
-  document.getElementById('inputField').disabled = false;
   document.getElementById('score').innerText = score+" points";
   timerValue = 60; 
   document.getElementById('timerDisplay').innerText = timerValue + 's';
+  document.getElementById('inputField').disabled = false;
   clearInterval(timerInterval); 
   timerInterval = setInterval(updateTimer, 1000);
   displayRandomEmoji();
